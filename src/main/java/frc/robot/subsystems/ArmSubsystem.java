4bddfc751a4d366a24ef623e8c197ae3ff5ad288
Daniel Quednow <dqued56@tscstudents.net>
2023-02-01 19:20:34 -0500

    Add motors to arm subsystem

--- src/main/java/frc/robot/subsystems/ArmSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmSubsystem.java
@@ -4,23 +4,33 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import static frc.robot.constants.HardwareMap.*;
+import static com.revrobotics.CANSparkMaxLowLevel.MotorType.*;
+
 public class ArmSubsystem extends SubsystemBase {
-  /** Creates a new ArmSubsystem. */
-  public ArmSubsystem() {
-  }
 
-  public void setElevatorPos(double height) {
-    // TODO: finish setElevatorPosition
-  }
+    private CANSparkMax elevatorLeftMotor = new CANSparkMax(ELEVATOR_LEFT_ID, kBrushless);
+    private CANSparkMax elevatorRightMotor = new CANSparkMax(ELEVATOR_RIGHT_ID, kBrushless);
+    private CANSparkMax armMotor = new CANSparkMax(ROTARY_ARM_ID, kBrushless);
 
-  public void setArm(double radians) {
-    // TODO: finish setArm
-  }
+    /** Creates a new ArmSubsystem. */
+    public ArmSubsystem() {
+    }
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    public void setElevatorPos(double height) {
+        // TODO: finish setElevatorPosition
+    }
+
+    public void setArm(double radians) {
+        // TODO: finish setArm
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
 }
