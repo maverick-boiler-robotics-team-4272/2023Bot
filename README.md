e90b84c3716ecc89a11fa904650181b23228cbda
Dannie226 <77417944+Dannie226@users.noreply.github.com>
2023-02-04 12:40:44 -0500

    Merge Limelight into main (#7)
    * Add LimelightHelpers
    
    Added LimelightHelpers from:
    https://github.com/LimelightVision/limelightlib-wpijava/blob/main/LimelightHelpers.java
    
    Chief Delphi Post:
    https://www.chiefdelphi.com/t/introducing-limelight-lib/425660
    
    * Fix typo
    
    * Move Limelight helper
    
    * Change the Limelight class to a wrapper
    
    Changed the Limelight class to wrap around LimelightHelpers. It is still
    constructable, and works the same as before, it just also has extra
    methods that it didn't have before.
    
    * Fix bugs that came up in testing

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 2023Bot
+
+## Limelight Lib
+LimelightHelpers Code Source:
+https://github.com/LimelightVision/limelightlib-wpijava/blob/main/LimelightHelpers.java
+
+Chief Delphi Post:
+https://www.chiefdelphi.com/t/introducing-limelight-lib/425660
---
+++ src/main/java/frc/robot/limelight/Limelight.java
@@ -0,0 +1,79 @@
+package frc.robot.limelight;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+
+import static frc.robot.constants.UniversalConstants.*;
+
+public final class Limelight {
+    public enum LEDMode {
+        kPipeline,
+        kOff,
+        kBlink,
+        kOn
+    }
+
+    private final String tableName;
+    
+    private static final Map<String, Limelight> limelightMap = new HashMap<>();
+
+    private Limelight(String tableName) {
+        if(tableName == "" || tableName == null) {
+            tableName = "limelight";
+        } else {
+            tableName = "limelight-" + tableName;
+        }
+
+        this.tableName = tableName;
+    }
+
+    public double[] getBotPose() {
+        double[] pose = LimelightHelpers.getBotpose(tableName);
+        if(pose.length != 6) return new double[6];
+        return pose;
+    }
+
+    public double[] getBotPoseInTargetSpace() {
+        double[] pose = LimelightHelpers.getBotPose_TargetSpace(tableName);
+        if(pose.length != 6) return new double[6];
+        return pose;
+    }
+
+    public Pose2d getRobotPose() {
+        double[] pose = getBotPose();
+        
+        return new Pose2d(pose[0] + FIELD_HALF_WIDTH, pose[1] + FIELD_HALF_HEIGHT, Rotation2d.fromDegrees(pose[5]));
+    }
+
+    public boolean isValidTarget() {
+        return LimelightHelpers.getLimelightNTDouble(tableName, "tv") != 0.0;
+    }
+
+    public double getTX() {
+        return LimelightHelpers.getTX(tableName);
+    }
+
+    public double getTY() {
+        return LimelightHelpers.getTY(tableName);
+    }
+
+    public void setCropWindow(double cropXMin, double cropXMax, double cropYMin, double cropYMax) {
+        LimelightHelpers.setCropWindow(tableName, cropXMin, cropXMax, cropYMin, cropYMax);
+    }
+
+    public void setLEDMode(LEDMode mode) {
+        if(mode == null) return;
+        LimelightHelpers.setLimelightNTDouble(tableName, "ledMode", mode.ordinal());
+    }
+
+    public static Limelight getLimelight(String name) {
+        if(!limelightMap.containsKey(name)) {
+            limelightMap.put(name, new Limelight(name));
+        }
+
+        return limelightMap.get(name);
+    }
+}
