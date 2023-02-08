be67861407b0077032455d3c8754eddaa88f5df4
Daniel Quednow <dqued56@tscstudents.net>
2023-02-07 19:30:06 -0500

    Change paths and loading
    Changed the paths to have names that match what they are doing.
    Added all the new paths to TrajectoryContainer
    Added acceleration constants to DrivetrainConstants

--- src/main/java/frc/robot/utils/TrajectoryContainer.java
+++ src/main/java/frc/robot/utils/TrajectoryContainer.java
@@ -8,9 +8,15 @@ import static frc.robot.constants.RobotConstants.DrivetrainConstants.*;
 public class TrajectoryContainer {
     public static TrajectoryContainer GLOBAL_SELECTED_TRAJECTORIES = null;
 
-    public final PathPlannerTrajectory TEST_PATH;
+    public final PathPlannerTrajectory FORWARD_PATH;
+    public final PathPlannerTrajectory BACKWARD_PATH;
+    public final PathPlannerTrajectory CHARGE_CIRCLE;
+    public final PathPlannerTrajectory CHARGE_END;
 
     public TrajectoryContainer(String prefix) {
-        TEST_PATH = PathPlanner.loadPath(prefix + " April Run", MAX_TRANS_SPEED, 2.0);
+        FORWARD_PATH = PathPlanner.loadPath(prefix + " April Run For", MAX_TRANS_SPEED, MAX_TRANS_ACCEL);
+        BACKWARD_PATH = PathPlanner.loadPath(prefix + " April Run Back", MAX_TRANS_SPEED, MAX_TRANS_ACCEL);
+        CHARGE_CIRCLE = PathPlanner.loadPath(prefix + " Charge Circle", MAX_TRANS_SPEED, MAX_TRANS_ACCEL);
+        CHARGE_END = PathPlanner.loadPath(prefix + " Charge End", MAX_TRANS_SPEED, MAX_TRANS_ACCEL);
     }
 }
