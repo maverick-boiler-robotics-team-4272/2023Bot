8e3424af389c053c00875195eb1c708209e39820
Daniel Quednow <dqued56@tscstudents.net>
2023-02-07 19:34:48 -0500

    Update auto commands
    Updated AprilRunCommand to use the new path names
    Created a ChargeCircleCommand to use the new paths
    Upadted RobotContainer to add a new auto option of ChargeCircleCommand

--- src/main/java/frc/robot/commands/AprilRunCommand.java
+++ src/main/java/frc/robot/commands/AprilRunCommand.java
@@ -1,33 +1,15 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.Drivetrain;
 
 import static frc.robot.constants.AutoConstants.Paths.getGlobalTrajectories;
 
-import com.pathplanner.lib.server.PathPlannerServer;
-
 public class AprilRunCommand extends SequentialCommandGroup {
     public AprilRunCommand(Drivetrain drivetrain) {
         super(
-            new PathFollowState(drivetrain, getGlobalTrajectories().TEST_PATH),
-            new InstantCommand(() -> {
-                PathPlannerServer.sendPathFollowingData(new Pose2d(), new Pose2d());
-            }),
-            new WaitCommand(1.0),
-            new InstantCommand(() -> {
-                var state = getGlobalTrajectories().TEST_PATH.getEndState();
-
-                PathPlannerServer.sendPathFollowingData(
-                    new Pose2d(
-                        state.poseMeters.getTranslation(), 
-                        state.holonomicRotation),
-                    drivetrain.getRobotPose()
-                );
-            })
+            new PathFollowState(drivetrain, getGlobalTrajectories().FORWARD_PATH),
+            new PathFollowState(drivetrain, getGlobalTrajectories().BACKWARD_PATH)
         );
     }
 }
