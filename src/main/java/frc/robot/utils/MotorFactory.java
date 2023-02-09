74411e4aec3b890acf3e9fa8d8a8c4dc79298dce
Daniel Quednow <dqued56@tscstudents.net>
2023-02-09 09:50:08 -0500

    Add PIDParameters class

--- src/main/java/frc/robot/utils/MotorFactory.java
+++ src/main/java/frc/robot/utils/MotorFactory.java
@@ -1,6 +1,28 @@
 package frc.robot.utils;
 
 public class MotorFactory {
+    public static class PIDParameters {
+        public final double kP;
+        public final double kI;
+        public final double kD;
+        public final double kF;
+
+        public PIDParameters(double kP, double kI, double kD, double kF) {
+            this.kP = kP;
+            this.kI = kI;
+            this.kD = kD;
+            this.kF = kF;
+        }
+
+        public PIDParameters(double kP, double kI, double kD) {
+            this(kP, kI, kD, 0.0);
+        }
+
+        public PIDParameters() {
+            this(1.0, 0.0, 0.0, 0.0);
+        }
+    }
+
     private MotorFactory() {
         throw new IllegalAccessError("Motor Factory is a utility class");
     }
