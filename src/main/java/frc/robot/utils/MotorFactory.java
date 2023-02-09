c72a7051ac51f051bc570bcf3893a910785c5896
Daniel Quednow <dqued56@tscstudents.net>
2023-02-09 09:52:20 -0500

    Add base spark max creation method

--- src/main/java/frc/robot/utils/MotorFactory.java
+++ src/main/java/frc/robot/utils/MotorFactory.java
@@ -1,5 +1,11 @@
 package frc.robot.utils;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import static frc.robot.constants.UniversalConstants.NOMINAL_VOLTAGE;
+
 public class MotorFactory {
     public static class PIDParameters {
         public final double kP;
@@ -25,5 +31,14 @@ public class MotorFactory {
 
     private MotorFactory() {
         throw new IllegalAccessError("Motor Factory is a utility class");
+    }
+
+    public CANSparkMax createSparkMax(int id) {
+        CANSparkMax spark = new CANSparkMax(id, MotorType.kBrushless);
+
+        spark.enableVoltageCompensation(NOMINAL_VOLTAGE);
+        spark.setIdleMode(IdleMode.kBrake);
+
+        return spark;
     }
 }
