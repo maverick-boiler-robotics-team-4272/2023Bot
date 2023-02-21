d40960562a88fb1b761e0e89c75f64c89a5ace48
Daniel Quednow <dqued56@tscstudents.net>
2023-02-20 19:38:26 -0500

    Add command for a one cone auto

---
+++ src/main/java/frc/robot/commands/OneConeCommand.java
@@ -0,0 +1,28 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.constants.RobotConstants.ArmSubsystemConstants.ArmSetpoints;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.ClawSubsystem;
+import frc.robot.subsystems.Drivetrain;
+
+import static frc.robot.constants.AutoConstants.Paths.getGlobalTrajectories;
+
+public class OneConeCommand extends SequentialCommandGroup {
+    public OneConeCommand(Drivetrain drivetrain, ArmSubsystem arm, ClawSubsystem claw) {
+        super(
+            new ArmSetpointCommand(arm, ArmSetpoints.HIGH_CONE),
+            new ParallelRaceGroup(
+                new ConeGrabState(claw, () -> -0.5),
+                new WaitCommand(1)
+            ),
+            new ParallelCommandGroup(
+                new ArmSetpointCommand(arm, ArmSetpoints.HOME),
+                new PathFollowState(drivetrain, getGlobalTrajectories().ONE_CONE_PATH)
+            )
+        );
+    }
+}
