853ca9b47bd0d134f320da287dd94a8b4765c36a
Daniel Quednow <dqued56@tscstudents.net>
2023-02-23 18:52:19 -0500

    Add a command for two cones
    Made a command for placing the first cone, and moving over to the
    second cone

---
+++ src/main/java/frc/robot/commands/TwoConeCommand.java
@@ -0,0 +1,32 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.constants.RobotConstants.ArmSubsystemConstants.ArmSetpoints;
+import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.subsystems.ClawSubsystem;
+import frc.robot.subsystems.Drivetrain;
+
+import static frc.robot.constants.AutoConstants.Paths.getGlobalTrajectories;
+
+public class TwoConeCommand extends SequentialCommandGroup {
+    public TwoConeCommand(Drivetrain drivetrain, ArmSubsystem arm, ClawSubsystem claw) {
+        super(
+            new ParallelRaceGroup(
+                new ArmSetpointState(arm, ArmSetpoints.HIGH_CONE),
+                new ConeGrabState(claw, () -> 0.1)
+            ),
+            new ParallelRaceGroup(
+                new ConeGrabState(claw, () -> -0.5),
+                new WaitCommand(0.5)
+            ),
+            new ParallelCommandGroup(
+                new ArmSetpointState(arm, ArmSetpoints.HOME),
+                new PathFollowState(drivetrain, getGlobalTrajectories().ONE_CONE_PATH)
+            ),
+            new PathFollowState(drivetrain, getGlobalTrajectories().TWO_CONE_PATH, false)
+        );
+    }
+}
