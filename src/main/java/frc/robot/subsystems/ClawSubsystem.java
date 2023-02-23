e009d739f76d88fba81328f705f471ee54e7ccff
Daniel Quednow <dqued56@tscstudents.net>
2023-02-22 20:02:18 -0500

    Use MotorBuilder in ClawSubsystem

--- src/main/java/frc/robot/subsystems/ClawSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClawSubsystem.java
@@ -5,23 +5,22 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utils.MotorBuilder;
 
 import static frc.robot.constants.HardwareMap.*;
 
 public class ClawSubsystem extends SubsystemBase {
-    private CANSparkMax clawLeaderMotor = new CANSparkMax(CLAW_RIGHT_ID, MotorType.kBrushless);
-    private CANSparkMax clawFollowerMotor = new CANSparkMax(CLAW_LEFT_ID, MotorType.kBrushless);
+    private CANSparkMax clawLeaderMotor;
+    private CANSparkMax clawFollowerMotor;
 
     /** Creates a new ClawSubsystem. */
     public ClawSubsystem() {
-        clawLeaderMotor.restoreFactoryDefaults();
-        clawFollowerMotor.restoreFactoryDefaults();
-
-        clawFollowerMotor.setSmartCurrentLimit(20);
-        clawLeaderMotor.setSmartCurrentLimit(20);
+        clawLeaderMotor = MotorBuilder.createWithDefaults(CLAW_RIGHT_ID)
+            .build();
+        clawFollowerMotor = MotorBuilder.createWithDefaults(CLAW_LEFT_ID)
+            .build();
     }
 
     @Override
