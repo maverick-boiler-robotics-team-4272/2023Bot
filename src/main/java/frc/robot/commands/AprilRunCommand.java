4133fcd68bd9b8f8151fbda85b4399f04572adf8
Daniel Quednow <dqued56@tscstudents.net>
2023-02-23 19:04:11 -0500

    Move drivetrain objects
    Moved all drivetrain related classes (drivetrain class and related
    states) to follow the following folder structure
    subsystems:
      subsystem named folder:
        subsystem class
        states folder:
          all states for that subsystem

--- src/main/java/frc/robot/commands/AprilRunCommand.java
+++ src/main/java/frc/robot/commands/AprilRunCommand.java
@@ -1,7 +1,8 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.drivetrain.Drivetrain;
+import frc.robot.subsystems.drivetrain.states.PathFollowState;
 
 import static frc.robot.constants.AutoConstants.Paths.getGlobalTrajectories;
 
