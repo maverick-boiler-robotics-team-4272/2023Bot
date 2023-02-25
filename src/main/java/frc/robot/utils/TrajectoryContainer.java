2fa53c3e8ae937a25501c89d6a4d053659020569
Daniel Quednow <dqued56@tscstudents.net>
2023-02-25 09:33:04 -0500

    Add path for picking up second cone

--- src/main/java/frc/robot/utils/TrajectoryContainer.java
+++ src/main/java/frc/robot/utils/TrajectoryContainer.java
@@ -11,9 +11,11 @@ public class TrajectoryContainer {
 
     public final PathPlannerTrajectory ONE_CONE_PATH;
     public final PathPlannerTrajectory TWO_CONE_PATH;
+    public final PathPlannerTrajectory TWO_CONE_PICKUP_PATH;
 
     public TrajectoryContainer(String prefix) {
         ONE_CONE_PATH = PathPlanner.loadPath(prefix + " One Cone", MAX_TRANS_SPEED, MAX_TRANS_ACCEL);
         TWO_CONE_PATH = PathPlanner.loadPath(prefix + " Two Cone", MAX_TRANS_SPEED, MAX_TRANS_ACCEL);
+        TWO_CONE_PICKUP_PATH = PathPlanner.loadPath(prefix + " Two Cone Pickup", 0.75, 1.0);
     }
 }
