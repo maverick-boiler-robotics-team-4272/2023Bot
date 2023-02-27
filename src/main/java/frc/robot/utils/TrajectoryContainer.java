92f4c84361cc5e494a549927505e03e457f476ea
alohr86 <alorh86@tscstudents.net>
2023-02-27 18:57:15 -0500

    Add two cone return path

--- src/main/java/frc/robot/utils/TrajectoryContainer.java
+++ src/main/java/frc/robot/utils/TrajectoryContainer.java
@@ -12,15 +12,11 @@ public class TrajectoryContainer {
 
     public final PathPlannerTrajectory ONE_CONE_PATH;
     public final PathPlannerTrajectory TWO_CONE_PATH;
-    public final PathPlannerTrajectory ONTO_CHARGESTATION_COMMUNITY;
-    public final PathPlannerTrajectory ONTO_CHARGESTATION_FIELD;
     public final PathPlannerTrajectory TWO_CONE_PATH_RETURN;
 
     public TrajectoryContainer(String prefix) {
         ONE_CONE_PATH = PathPlanner.loadPath(prefix + " One Cone", MAX_TRANS_SPEED, MAX_TRANS_ACCEL);
         TWO_CONE_PATH = PathPlanner.loadPath(prefix + " Two Cone", MAX_TRANS_SPEED, MAX_TRANS_ACCEL);
-        ONTO_CHARGESTATION_COMMUNITY = PathPlanner.loadPath(prefix + " Chargestation From Community", MAX_TRANS_SPEED, MAX_TRANS_ACCEL);
-        ONTO_CHARGESTATION_FIELD = PathPlanner.loadPath(prefix + " ChargeStation From Field", MAX_TRANS_SPEED, MAX_TRANS_ACCEL);
-        TWO_CONE_PATH_RETURN = PathPlanner.loadPath(prefix + " Two Cone Score", MAX_TRANS_SPEED, MAX_TRANS_ACCEL);
+        TWO_CONE_PATH_RETURN = PathPlanner.loadPath(prefix + " Two Cone Return", MAX_TRANS_SPEED, MAX_TRANS_ACCEL);
     }
 }
