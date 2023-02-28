ce29a50d0b23cc7a7d40a0b59951dcb38a76c6ad
Dannie226 <77417944+Dannie226@users.noreply.github.com>
2023-02-28 17:55:35 -0500

    Motor builder (#14)
    * Create MotorFactory utility class
    
    * Add PIDParameters class
    
    * Add base spark max creation method
    
    * Add spark max creation methods with PID parameters
    
    * Decide on using a builder over a factory
    
    * Implement MotorBuilder
    
    * Merge branch 'main' into MotorFactory
    
    * Add withIdleMode method
    
    Added a withIdleMode method to set the idle mode, and added it to
    the createWithDefaults method
    
    * Add withInversion and asFollower methods
    
    * Add withPosition method
    
    * Use MotorBuilder in SwerveModule
    
    * Use MotorBuilder in ClawSubsystem
    
    * Rework asFollower and add other PID methods
    
    Reworked asFollower to just set the motor as a follower, but still
    return a builder so other parameters can be set.
    
    * Used MotorBuilder in ArmSubsystem.java
    
    * Merge main into MotorFactory
    
    Squashed commit of the following:
    
    commit 64ebcb106f7648d6427376182d3e4cd7550384f8
    Author: Dannie226 <77417944+Dannie226@users.noreply.github.com>
    Date:   Thu Feb 23 19:57:31 2023 -0500
    
        Lidar (#17)
    
        * Add lidar utility class
    
        * Add lidars to intake subsystem
    
        * Add methods to use the lidar as a beam break
    
        * Create HoldState
    
        Created a new hold state that uses the intake lidars to figure out
        whether it is holding a cone or a cube, and runs the correct grab method
        to keep a hold of the cone or cube.
        Removed the other hold states because they are no longer necessary.
    
    commit 81b6048966c914a331e5a764506816b9ea6ffd9f
    Author: Dannie226 <77417944+Dannie226@users.noreply.github.com>
    Date:   Thu Feb 23 19:34:14 2023 -0500
    
        Intake states (#16)
    
        * Fix a typo in intake subsystem name
    
        * Add stop motors method to intake subsystem
    
        * Add eject states
    
        * Add hold states
    
        According to the spec there is only one hold state. With the current
        state of the code, there are no lidars, so, for now, we have to settle
        for having two states, and manually chosing between them.
        This will change in the future when lidars are implemented in code
    
    commit 62ca50eda5cb6d0ad7c34a67525960e203f20783
    Author: Dannie226 <77417944+Dannie226@users.noreply.github.com>
    Date:   Thu Feb 23 19:11:35 2023 -0500
    
        State ordering (#15)
    
        * Move drivetrain objects
    
        Moved all drivetrain related classes (drivetrain class and related
        states) to follow the following folder structure
        subsystems:
          subsystem named folder:
            subsystem class
            states folder:
              all states for that subsystem
    
        * Move arm objects to follow subsystem structure
    
        * move ClawSubsystem to intake subfolder and rename
    
        * Move intake states

--- src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeSubsystem.java
@@ -5,28 +5,27 @@
 package frc.robot.subsystems.intake;
 
 import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.utils.Lidar;
+import frc.robot.utils.MotorBuilder;
 
 import static frc.robot.constants.HardwareMap.*;
 import static frc.robot.constants.TelemetryConstants.ShuffleboardTables.*;
 
 public class IntakeSubsystem extends SubsystemBase {
-    private CANSparkMax clawLeaderMotor = new CANSparkMax(CLAW_RIGHT_ID, MotorType.kBrushless);
-    private CANSparkMax clawFollowerMotor = new CANSparkMax(CLAW_LEFT_ID, MotorType.kBrushless);
+    private CANSparkMax clawLeaderMotor;
+    private CANSparkMax clawFollowerMotor;
 
     private Lidar coneLidar = new Lidar(11);
     private Lidar cubeLidar = new Lidar(10);
 
     /** Creates a new ClawSubsystem. */
     public IntakeSubsystem() {
-        clawLeaderMotor.restoreFactoryDefaults();
-        clawFollowerMotor.restoreFactoryDefaults();
-
-        clawFollowerMotor.setSmartCurrentLimit(20);
-        clawLeaderMotor.setSmartCurrentLimit(20);
+        clawLeaderMotor = MotorBuilder.createWithDefaults(CLAW_RIGHT_ID)
+            .build();
+        clawFollowerMotor = MotorBuilder.createWithDefaults(CLAW_LEFT_ID)
+            .build();
     }
 
     public void setConeCurrentLimits() {
