98783fbda2e0eea70976144cc08c82243725b4e3
alohr86 <alorh86@tscstudents.net>
2023-03-06 17:42:37 -0500

    Simplify waiting

--- src/main/java/frc/robot/commands/OneConeCharge.java
+++ src/main/java/frc/robot/commands/OneConeCharge.java
@@ -1,16 +1,10 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
-import frc.robot.constants.TelemetryConstants;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.constants.RobotConstants.ArmSubsystemConstants.ArmSetpoints;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.states.ArmSetpointState;
@@ -19,13 +13,7 @@ import frc.robot.subsystems.drivetrain.states.DriveState;
 import frc.robot.subsystems.drivetrain.states.PathFollowState;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.intake.states.ConeGrabState;
-import frc.robot.utils.Pigeon;
-import frc.robot.utils.ShuffleboardTable;
-
 import static frc.robot.constants.AutoConstants.Paths.getGlobalTrajectories;
-
-import java.util.HashSet;
-import java.util.Set;
 
 public class OneConeCharge extends SequentialCommandGroup{
     public OneConeCharge(Drivetrain drivetrain, ArmSubsystem arm, IntakeSubsystem intake) {
@@ -42,19 +30,7 @@ public class OneConeCharge extends SequentialCommandGroup{
             ),
             new ParallelRaceGroup(
                 new DriveState(drivetrain, 0.0, 0.1, 0.00),
-                new Command() {
-                    private Set<Subsystem> s = new HashSet<>();
-
-                    @Override
-                    public Set<Subsystem> getRequirements() {
-                        return s;
-                    }
-
-                    @Override
-                    public boolean isFinished() {
-                        return drivetrain.getGyroscope().getPitch() < 2;
-                    }
-                }
+                new WaitUntilCommand(() -> drivetrain.getGyroscope().getPitch() < 2)
             ),
             new InstantCommand(
                 drivetrain::xConfig, drivetrain
