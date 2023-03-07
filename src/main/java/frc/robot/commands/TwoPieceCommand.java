e81f46672ef3f86d8132ea99e74011196125659c
Daniel Quednow <dqued56@tscstudents.net>
2023-03-06 20:23:58 -0500

    Start bug fixing on two piece auto

--- src/main/java/frc/robot/commands/TwoPieceCommand.java
+++ src/main/java/frc/robot/commands/TwoPieceCommand.java
@@ -2,6 +2,7 @@ package frc.robot.commands;
 
 import com.pathplanner.lib.commands.FollowPathWithEvents;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.states.ArmSetpointState;
@@ -10,6 +11,7 @@ import frc.robot.subsystems.drivetrain.states.PathFollowState;
 import frc.robot.subsystems.drivetrain.states.ResetPoseState;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.intake.states.ConeEjectState;
+import frc.robot.subsystems.intake.states.ConeGrabState;
 
 import java.util.Map;
 
@@ -22,7 +24,7 @@ public class TwoPieceCommand extends SequentialCommandGroup {
     public TwoPieceCommand(Drivetrain drivetrain, ArmSubsystem arm, IntakeSubsystem intake) {
         super(
             new ArmSetpointState(arm, HIGH_CONE),
-            new ConeEjectState(intake, () -> 0.1).withTimeout(0.2),
+            new ConeEjectState(intake, () -> 1.0).withTimeout(1.0),
             new ArmSetpointState(arm, HOME),
             new FollowPathWithEvents(
                 new PathFollowState(drivetrain, getGlobalTrajectories().TWO_PIECE_PATH),
@@ -31,7 +33,10 @@ public class TwoPieceCommand extends SequentialCommandGroup {
                     "reset od",
                     new ResetPoseState(drivetrain, CENTER),
                     "drop intake",
-                    new ArmSetpointState(arm, GROUND_CONE).repeatedly(),
+                    new ParallelCommandGroup(
+                        new ArmSetpointState(arm, GROUND_CONE),
+                        new ConeGrabState(intake, () -> 0.5).withTimeout(5.0)
+                    ),
                     "stow",
                     new ArmSetpointState(arm, HOME)
                 )
