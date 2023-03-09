087cdb654cc2a96c998ca776e04a77730a47e0bc
Daniel Quednow <dqued56@tscstudents.net>
2023-03-08 20:12:04 -0500

    Load new paths

--- src/main/java/frc/robot/utils/TrajectoryContainer.java
+++ src/main/java/frc/robot/utils/TrajectoryContainer.java
@@ -11,15 +11,19 @@ public class TrajectoryContainer {
     public final PathPlannerTrajectory TWO_CONE_PATH;
     public final PathPlannerTrajectory TWO_CONE_PATH_RETURN;
     public final PathPlannerTrajectory CHARGE_STATION_CROSSOVER;
-    public final PathPlannerTrajectory TWO_PIECE_PATH;
+    public final PathPlannerTrajectory TWO_PIECE_GRAB;
+    public final PathPlannerTrajectory TWO_PIECE_PLACE;
     public final PathPlannerTrajectory TWO_PIECE_CHARGE;
+    public final PathPlannerTrajectory TUNE;
 
     public TrajectoryContainer(String prefix) {
         ONE_CONE_PATH = PathPlanner.loadPath(prefix + " One Cone", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
         TWO_CONE_PATH = PathPlanner.loadPath(prefix + " Two Cone", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
         TWO_CONE_PATH_RETURN = PathPlanner.loadPath(prefix + " Two Cone Return", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
         CHARGE_STATION_CROSSOVER = PathPlanner.loadPath(prefix + " Charge Crossover", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
-        TWO_PIECE_PATH = PathPlanner.loadPath(prefix + " Two Piece", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
+        TWO_PIECE_GRAB = PathPlanner.loadPath(prefix + " Two Piece Grab", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
+        TWO_PIECE_PLACE = PathPlanner.loadPath(prefix + " Two Piece Place", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
         TWO_PIECE_CHARGE = PathPlanner.loadPath(prefix + " Two Charge", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
+        TUNE = PathPlanner.loadPath(prefix + " Tune", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
     }
 }
