a533376c8c0544bf3c7275617ca52d032f531f6c
Daniel Quednow <dqued56@tscstudents.net>
2023-03-08 20:12:22 -0500

    Re-work two piece command for new paths

--- src/main/java/frc/robot/commands/TwoPieceCommand.java
+++ src/main/java/frc/robot/commands/TwoPieceCommand.java
@@ -3,13 +3,12 @@ package frc.robot.commands;
 import com.pathplanner.lib.commands.FollowPathWithEvents;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.states.ArmSetpointState;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.drivetrain.states.PathFollowState;
-import frc.robot.subsystems.drivetrain.states.ResetPoseState;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.intake.states.ConeEjectState;
 import frc.robot.subsystems.intake.states.ConeGrabState;
@@ -18,34 +17,40 @@ import java.util.Map;
 
 import static frc.robot.constants.AutoConstants.Paths.getGlobalTrajectories;
 import static frc.robot.constants.RobotConstants.ArmSubsystemConstants.ArmSetpoints.*;
-import static frc.robot.constants.TelemetryConstants.Limelights.CENTER;
-
 
 public class TwoPieceCommand extends SequentialCommandGroup {
     public TwoPieceCommand(Drivetrain drivetrain, ArmSubsystem arm, IntakeSubsystem intake) {
         super(
             new ArmSetpointState(arm, HIGH_CONE),
-            new WaitCommand(2.0),
-            // new ConeEjectState(intake, () -> 1.0).withTimeout(1.0)
+            new ConeEjectState(intake, () -> 0.5).withTimeout(0.2),
+            new ArmSetpointState(arm, HOME),
+            new FollowPathWithEvents(
+                new PathFollowState(drivetrain, getGlobalTrajectories().TWO_PIECE_GRAB),
+                getGlobalTrajectories().TWO_PIECE_GRAB.getMarkers(),
+                Map.of(
+                    "drop intake",
+                    new SequentialCommandGroup(
+                        new ParallelCommandGroup(
+                            new ArmSetpointState(arm, GROUND_CONE),
+                            new ConeGrabState(intake, () -> 0.5).withTimeout(0.75)
+                        ),
+                        new ParallelCommandGroup(
+                            new ArmSetpointState(arm, HOME),
+                            new ConeGrabState(intake, () -> 0.1)
+                        )
+                    )
+                )
+            ),
+            new ArmSetpointState(arm, HOME),
+            new ParallelRaceGroup(
+                new SequentialCommandGroup(
+                    new PathFollowState(drivetrain, getGlobalTrajectories().TWO_PIECE_PLACE),
+                    new ArmSetpointState(arm, HIGH_CONE)
+                ),
+                new ConeGrabState(intake, () -> 0.1)
+            ),
+            new ConeEjectState(intake, () -> 0.5),
             new ArmSetpointState(arm, HOME)
-            // new FollowPathWithEvents(
-            //     new PathFollowState(drivetrain, getGlobalTrajectories().TWO_PIECE_PATH),
-            //     getGlobalTrajectories().TWO_PIECE_PATH.getMarkers(),
-            //     Map.of(
-            //         // "reset od",
-            //         // new ResetPoseState(drivetrain, CENTER),
-            //         "drop intake",
-            //         new ParallelCommandGroup(
-            //             new ArmSetpointState(arm, GROUND_CONE),
-            //             new ConeGrabState(intake, () -> 0.5).withTimeout(5.0)
-            //         ),
-            //         "stow",
-            //         new ArmSetpointState(arm, HOME)
-            //     )
-            // ),
-            // new ArmSetpointState(arm, HIGH_CONE),
-            // new ConeEjectState(intake, () -> 0.1).withTimeout(0.2),
-            // new ArmSetpointState(arm, HOME)
         );
     }
 }
