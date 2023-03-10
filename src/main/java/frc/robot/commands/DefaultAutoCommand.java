ef71137575216c794acd2cc27e41d4485273de96
Daniel Quednow <dqued56@tscstudents.net>
2023-03-10 10:16:09 -0500

    Change auto command
    Changed default auto command to just place a cone.
    Kept the one cone back up as a separate thing so we can still use it if
    we need to

--- src/main/java/frc/robot/commands/DefaultAutoCommand.java
+++ src/main/java/frc/robot/commands/DefaultAutoCommand.java
@@ -1,27 +1,18 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.constants.RobotConstants.ArmSubsystemConstants.ArmSetpoints;
-import frc.robot.constants.TelemetryConstants;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.states.ArmSetpointState;
-import frc.robot.subsystems.drivetrain.Drivetrain;
-import frc.robot.subsystems.drivetrain.states.DriveState;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.intake.states.ConeEjectState;
 
 public class DefaultAutoCommand extends SequentialCommandGroup {
-    public DefaultAutoCommand(Drivetrain drivetrain, ArmSubsystem arm, IntakeSubsystem intake) {
+    public DefaultAutoCommand(ArmSubsystem arm, IntakeSubsystem intake) {
         addCommands(
             new ArmSetpointState(arm, ArmSetpoints.HIGH_CONE),
             new ConeEjectState(intake, () -> 0.1).withTimeout(0.5),
-            new ArmSetpointState(arm, ArmSetpoints.HOME),
-            new InstantCommand(() -> {
-                drivetrain.getGyroscope().setRotation(Rotation2d.fromDegrees(TelemetryConstants.FMS.RED_ALLIANCE.get() ? 0 : 180));
-            }, drivetrain),
-            new DriveState(drivetrain, () -> 0.0, () -> 0.3, () -> 0.0).withTimeout(1.5)
+            new ArmSetpointState(arm, ArmSetpoints.HOME)
         );
     }
 }
