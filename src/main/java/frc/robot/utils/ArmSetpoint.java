ff4f6a8b3d4d12fa4d15986db3ff44050f7c0be0
Daniel Quednow <dqued56@tscstudents.net>
2023-03-13 10:01:04 -0400

    Add create arbitrary method to arm setpoint

--- src/main/java/frc/robot/utils/ArmSetpoint.java
+++ src/main/java/frc/robot/utils/ArmSetpoint.java
@@ -6,4 +6,28 @@ public interface ArmSetpoint {
     public double getElevatorHeight();
     public Rotation2d getArmAngle();
     public boolean getSafetyOverride();
+
+    public static ArmSetpoint createArbitrary(double elevatorHeight, Rotation2d armAngle) {
+        return new ArmSetpoint() {
+            @Override
+            public double getElevatorHeight() {
+                return elevatorHeight;
+            }
+
+            @Override
+            public Rotation2d getArmAngle() {
+                return armAngle;
+            }
+
+            @Override
+            public boolean getSafetyOverride() {
+                return false;
+            }
+
+            @Override
+            public String toString() {
+                return String.format("ArbitrarySetpoint(height = %.2f, angle = %.2fdeg", elevatorHeight, armAngle);
+            }
+        };
+    }
 }
