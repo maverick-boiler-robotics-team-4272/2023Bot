3f42e0d288b5ee6e49e88e197a25af2faf0256e4
Daniel Quednow <dqued56@tscstudents.net>
2023-03-14 09:46:09 -0400

    Add a state to play the rainbow animation

---
+++ src/main/java/frc/robot/subsystems/candle/states/RainbowState.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems.candle.states;
+
+import com.ctre.phoenix.led.RainbowAnimation;
+
+import frc.robot.subsystems.candle.Candle;
+import frc.team4272.globals.State;
+
+public class RainbowState extends State<Candle> {
+    public RainbowState(Candle candle) {
+        super(candle);
+    }
+
+    @Override
+    public void initialize() {
+        requiredSubsystem.animate(
+            new RainbowAnimation()
+        );
+    }
+}
