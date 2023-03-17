a2f256cb96b68d3ec995b1cf9b2f1f4204930da7
Daniel Quednow <dqued56@tscstudents.net>
2023-03-17 09:32:58 -0400

    Make the multi cube command place a cube
    Changed other bits and bobs to make the command use cubes instead of
    cones
    Have states going to mid instead of high, because quantity matters more
    than quality in auto
    Use the stow intake marker to stow the intake

--- src/main/java/frc/robot/commands/ConeMultiCubeCommand.java
+++ src/main/java/frc/robot/commands/ConeMultiCubeCommand.java
@@ -7,33 +7,38 @@ import com.pathplanner.lib.commands.FollowPathWithEvents;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.arm.states.ArmSetpointState;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.drivetrain.states.PathFollowState;
+import frc.robot.subsystems.drivetrain.states.ResetPoseState;
 import frc.robot.subsystems.intake.IntakeSubsystem;
+import frc.robot.subsystems.intake.states.ConeEjectState;
 import frc.robot.subsystems.intake.states.CubeEjectState;
 import frc.robot.subsystems.intake.states.CubeGrabState;
 
 import static frc.robot.constants.RobotConstants.ArmSubsystemConstants.ArmSetpoints.*;
 import static frc.robot.constants.AutoConstants.Paths.getGlobalTrajectories;
+import static frc.robot.constants.TelemetryConstants.Limelights.*;
 
 public class ConeMultiCubeCommand extends SequentialCommandGroup {
     public ConeMultiCubeCommand(Drivetrain drivetrain, ArmSubsystem arm, IntakeSubsystem intake) {
         addCommands(
-            new ArmSetpointState(arm, HIGH_CONE),
-            new CubeEjectState(intake, () -> 0.9).withTimeout(0.5),
-            new ArmSetpointState(arm, STOWED),
+            new ArmSetpointState(arm, MID_CONE),
+            new ConeEjectState(intake, () -> 0.9).withTimeout(0.3),
             new FollowPathWithEvents(
                 new PathFollowState(drivetrain, getGlobalTrajectories().TWO_PIECE_GRAB),
                 getGlobalTrajectories().TWO_PIECE_GRAB.getMarkers(),
                 Map.of(
+                    "stow intake",
+                    new ArmSetpointState(arm, STOWED),
                     "drop intake",
                     new SequentialCommandGroup(
                         new ParallelCommandGroup(
-                            new ArmSetpointState(arm, GROUND_CONE),
+                            new ArmSetpointState(arm, GROUND_CUBE),
                             new CubeGrabState(intake, () -> 1.0).withTimeout(2.0)
                         ),
                         new ParallelCommandGroup(
@@ -45,14 +50,23 @@ public class ConeMultiCubeCommand extends SequentialCommandGroup {
             ),
             new ArmSetpointState(arm, STOWED),
             new InstantCommand(() -> {
-                if(!intake.isConeLidarTripped()) {
+                if(!intake.isCubeLidarTripped()) {
                     CommandScheduler.getInstance().cancel(this);
                 }
             }),
             new ParallelRaceGroup(
                 new PathFollowState(drivetrain, getGlobalTrajectories().CUBE_PLACE, false),
                 new CubeGrabState(intake, () -> 0.3)
-            )
+            ),
+            new SequentialCommandGroup(
+                new ParallelDeadlineGroup(
+                    new ArmSetpointState(arm, MID_CUBE),
+                    new CubeGrabState(intake, () -> 0.5),
+                    new ResetPoseState(drivetrain, CENTER)
+                ),
+                new CubeEjectState(intake, () -> 0.4).withTimeout(0.2)
+            ),
+            new ArmSetpointState(arm, STOWED)
         );
     }
 }
