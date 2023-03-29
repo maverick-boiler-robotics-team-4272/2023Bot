e5ebc2d74a7f093e9ca7667ccb7d25f40661301d
Dannie226 <77417944+Dannie226@users.noreply.github.com>
2023-03-29 19:54:29 -0400

    Direction enum (#35)
    * Update to newest version
    
    * Remove direction enum
    
    * Use new enum from 4272 library

--- src/main/java/frc/robot/utils/XboxController.java
+++ src/main/java/frc/robot/utils/XboxController.java
@@ -3,26 +3,6 @@ package frc.robot.utils;
 import frc.team4272.controllers.Controller;
 
 public class XboxController extends Controller {
-    public static enum POVDirection {
-        UP(0),
-        UP_RIGHT(45),
-        RIGHT(90),
-        DOWN_RIGHT(135),
-        DOWN(180),
-        DOWN_LEFT(225),
-        LEFT(270),
-        UP_LEFT(315);
-
-        private final int povValue;
-        private POVDirection(int povValue) {
-            this.povValue = povValue;
-        }
-
-        public boolean matches(int povValue) {
-            return this.povValue == povValue;
-        }
-    }
-
     public XboxController(int port) {
         super(port);
 
