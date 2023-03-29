8b9196b76edd0b29996ef18f949660ea4f496bba
Daniel Quednow <dqued56@tscstudents.net>
2023-03-29 19:25:51 -0400

    Remove charge station cross over paths

--- src/main/java/frc/robot/utils/TrajectoryContainer.java
+++ src/main/java/frc/robot/utils/TrajectoryContainer.java
@@ -12,7 +12,6 @@ public class TrajectoryContainer {
 
     public final PathPlannerTrajectory ONE_CONE_PATH;
     public final PathPlannerTrajectory TWO_CONE_PATH;
-    public final PathPlannerTrajectory CHARGE_STATION_CROSSOVER;
     public final PathPlannerTrajectory TWO_PIECE_GRAB;
     public final PathPlannerTrajectory TWO_PIECE_PLACE;
     public final PathPlannerTrajectory TWO_PIECE_CHARGE;
@@ -25,7 +24,6 @@ public class TrajectoryContainer {
     public TrajectoryContainer(String prefix) {
         ONE_CONE_PATH = PathPlanner.loadPath(prefix + " One Cone", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
         TWO_CONE_PATH = PathPlanner.loadPath(prefix + " Two Cone", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
-        CHARGE_STATION_CROSSOVER = PathPlanner.loadPath(prefix + " Charge Crossover", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
         TWO_PIECE_GRAB = PathPlanner.loadPath(prefix + " Two Piece Grab", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
         TWO_PIECE_PLACE = PathPlanner.loadPath(prefix + " Two Piece Place", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
         TWO_PIECE_CHARGE = PathPlanner.loadPath(prefix + " Two Charge", MAX_AUTO_SPEED, MAX_AUTO_ACCEL);
