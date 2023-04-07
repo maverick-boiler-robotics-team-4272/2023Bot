eb12be15816e582ba6386fe2f24e15793e4294bf
Daniel Quednow <dqued56@tscstudents.net>
2023-04-07 09:53:29 -0400

    Change LED indexing
    Because of the fact that the cone lidar is so much less reliable, we
    are having less LEDs devoted to cone detection and transferring them to
    cube detection.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -198,11 +198,11 @@ public class RobotContainer {
 
     private void configureCandleSignaling() {
         new Trigger(intake::isCubeLidarTripped).whileTrue(
-            Commands.runEnd(() -> candle.setLEDs(72, 30, 128, 0, 128), () -> candle.turnOffLEDs(72, 30))
+            Commands.runEnd(() -> candle.setLEDs(40, 62, 128, 0, 128), () -> candle.turnOffLEDs(72, 30))
         );
 
         new Trigger(intake::isConeLidarTripped).whileTrue(
-            Commands.runEnd(() -> candle.setLEDs(8, 64, 255, 255, 0), () -> candle.turnOffLEDs(8, 64))
+            Commands.runEnd(() -> candle.setLEDs(8, 32, 255, 255, 0), () -> candle.turnOffLEDs(8, 64))
         );
     }
 
