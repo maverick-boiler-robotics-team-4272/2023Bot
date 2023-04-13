fdf7438618dd8efff127780d62a4b71889da3d3d
Daniel Quednow <dqued56@tscstudents.net>
2023-04-13 12:34:06 -0400

    Tweak blue three piece cable

--- src/main/java/frc/robot/commands/ThreePieceCableSide.java
+++ src/main/java/frc/robot/commands/ThreePieceCableSide.java
@@ -1,31 +1,22 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.constants.RobotConstants.ArmSubsystemConstants.ArmSetpoints;
-import frc.robot.constants.TelemetryConstants.Limelights;
-import frc.robot.limelight.Limelight;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.intake.states.ConeEjectState;
-import frc.robot.subsystems.intake.states.ConeGrabState;
 import frc.robot.subsystems.intake.states.CubeEjectState;
 import frc.robot.subsystems.intake.states.CubeGrabState;
-import frc.robot.utils.ArmSetpoint;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.subsystems.drivetrain.states.PathFollowState;
-import frc.robot.subsystems.drivetrain.states.ResetPoseState;
 import frc.robot.subsystems.arm.states.ArmSetpointState;
 
 import static frc.robot.constants.AutoConstants.Paths.getGlobalTrajectories;
 
 import java.util.Map;
-import java.util.concurrent.Delayed;
-
 import com.pathplanner.lib.commands.FollowPathWithEvents;
 
 public class ThreePieceCableSide extends SequentialCommandGroup {
