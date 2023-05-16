cd2d0403195da8bcc8a25ef612636bf5748c3c9d
Daniel Quednow <dqued56@tscstudents.net>
2023-05-16 19:04:05 -0400

    Bug fix the rotation speed
    Changed the method to use all three limelights, and to average them
    Changed the rotation pid controller to be continuous
    Added target validation to make sure garbage data isn't polled
    It's still a bit glitchy, so more debugging is to come

--- src/main/java/frc/robot/subsystems/drivetrain/states/HumanPlayerLineupState.java
+++ src/main/java/frc/robot/subsystems/drivetrain/states/HumanPlayerLineupState.java
@@ -3,7 +3,6 @@ package frc.robot.subsystems.drivetrain.states;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import frc.robot.constants.TelemetryConstants.Limelights;
 import frc.robot.subsystems.drivetrain.Drivetrain;
@@ -14,16 +13,49 @@ public class HumanPlayerLineupState extends DriveState {
 
     public HumanPlayerLineupState(Drivetrain drivetrain, DoubleSupplier xSpeed, DoubleSupplier ySpeed, PIDController rotationController) {
         super(drivetrain, xSpeed, ySpeed, () -> {
-            Rotation2d ang = Limelights.CENTER.getRobotPose().getRotation();
-            return rotationController.calculate(ang.getRadians());
+            double centerRot = MathUtils.euclideanModulo(Limelights.CENTER.getRobotPose().getRotation().getRadians(), 2 * Math.PI);
+            double leftRot = MathUtils.euclideanModulo(Limelights.LEFT.getRobotPose().getRotation().getRadians(), 2 * Math.PI);
+            double rightRot = MathUtils.euclideanModulo(Limelights.RIGHT.getRobotPose().getRotation().getRadians(), 2 * Math.PI);
+    
+            int n = 0;
+            double r = 0;
+    
+            if(Limelights.CENTER.isValidTarget()) {
+                r += centerRot;
+                n++;
+            }
+    
+            if(Limelights.LEFT.isValidTarget()) {
+                r += leftRot;
+                n++;
+            }
+    
+            if(Limelights.RIGHT.isValidTarget()) {
+                r += rightRot;
+                n++;
+            }
+            
+            if(n != 0) {
+                r /= n;
+                return -rotationController.calculate(r);
+            } else {
+                return 0;
+            }
+
         });
         this.rotationController = rotationController;
+        this.rotationController.enableContinuousInput(0, 2 * Math.PI);
     }
 
     @Override
     public void initialize() {
-        Pose2d currentRobotPose = Limelights.CENTER.getRobotPose();
-        double rads = MathUtils.euclideanModulo(currentRobotPose.getRotation().getRadians(), 2 * Math.PI);
+        rotationController.reset();
+
+        Rotation2d centerRot = Limelights.CENTER.getRobotPose().getRotation();
+        double rads = MathUtils.euclideanModulo(centerRot.getRadians(), 2 * Math.PI);
+        if(!Limelights.CENTER.isValidTarget()) {
+            rads = 0;
+        }
 
         if(Math.abs(rads - Math.PI) < Math.PI / 2) {
             rotationController.setSetpoint(Math.PI);
