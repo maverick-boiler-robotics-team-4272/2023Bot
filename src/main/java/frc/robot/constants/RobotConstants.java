0649740b55c3445397d91c475fa61360d7031620
Daniel Quednow <dqued56@tscstudents.net>
2023-06-27 18:58:00 -0400

    Fix rotary arm offset

--- src/main/java/frc/robot/constants/RobotConstants.java
+++ src/main/java/frc/robot/constants/RobotConstants.java
@@ -72,7 +72,7 @@ public class RobotConstants {
         public static class RotaryArmConstants {
             // Any offset less than 120 degrees is generally going to be bad
             // To fix this, reset encoder position or chain to make the offset greater than 120
-            public static final double ROTARY_ARM_OFFSET = IS_PRACTICE_BOT ? 192.0 : 263.0;
+            public static final double ROTARY_ARM_OFFSET = IS_PRACTICE_BOT ? 192.0 : 262.0;
 
             public static final double ROTARY_ARM_PID_P = IS_PRACTICE_BOT ? 0.012 : 0.014;
             public static final double ROTARY_ARM_PID_I = IS_PRACTICE_BOT ? 0.001 : 0.0;
