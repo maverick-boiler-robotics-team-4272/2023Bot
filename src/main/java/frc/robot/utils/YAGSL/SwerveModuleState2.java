382bb64af3f21688b6ba9cab0061c283ed3c5685
Daniel Quednow <dqued56@tscstudents.net>
2023-07-11 20:03:28 -0400

    Add YAGSL math

---
+++ src/main/java/frc/robot/utils/YAGSL/SwerveModuleState2.java
@@ -0,0 +1,106 @@
+package frc.robot.utils.YAGSL;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.team4272.globals.MathUtils;
+
+/**
+ * Second order kinematics swerve module state.
+ */
+public class SwerveModuleState2 extends SwerveModuleState
+{
+
+  /**
+   * Rad per sec
+   */
+  public double omegaRadPerSecond = 0;
+
+  /**
+   * Constructs a SwerveModuleState with zeros for speed and angle.
+   */
+  public SwerveModuleState2()
+  {
+    super();
+  }
+
+  /**
+   * Constructs a SwerveModuleState.
+   *
+   * @param speedMetersPerSecond The speed of the wheel of the module.
+   * @param angle                The angle of the module.
+   * @param omegaRadPerSecond    The angular velocity of the module.
+   */
+  public SwerveModuleState2(
+      double speedMetersPerSecond, Rotation2d angle, double omegaRadPerSecond)
+  {
+    super(speedMetersPerSecond, angle);
+    this.omegaRadPerSecond = omegaRadPerSecond;
+  }
+
+  /**
+   * Create a {@link SwerveModuleState2} based on the {@link SwerveModuleState} with the radians per second defined.
+   *
+   * @param state             First order kinematic module state.
+   * @param omegaRadPerSecond Module wheel angular rotation in radians per second.
+   */
+  public SwerveModuleState2(SwerveModuleState state, double omegaRadPerSecond)
+  {
+    super(state.speedMetersPerSecond, state.angle);
+    this.omegaRadPerSecond = omegaRadPerSecond;
+  }
+
+  /**
+   * Minimize the change in heading the desired swerve module state would require by potentially reversing the direction
+   * the wheel spins. If this is used with the PIDController class's continuous input functionality, the furthest a
+   * wheel will ever rotate is 90 degrees.
+   *
+   * @param desiredState                     The desired state.
+   * @param currentAngle                     The current module angle.
+   * @param lastState                        The last state of the module.
+   * @param moduleSteerFeedForwardClosedLoop The module feed forward closed loop for the angle motor.
+   * @return Optimized swerve module state.
+   */
+  public static SwerveModuleState2 optimize(SwerveModuleState2 desiredState, Rotation2d currentAngle,
+                                            SwerveModuleState2 lastState, double moduleSteerFeedForwardClosedLoop)
+  {
+    if (moduleSteerFeedForwardClosedLoop == 0)
+    {
+//    desiredState.angle = desiredState.angle.plus(Rotation2d.fromRadians(
+//        lastState.omegaRadPerSecond * moduleSteerFeedForwardClosedLoop * 0.065));
+//      return new SwerveModuleState2(SwerveModuleState.optimize(desiredState, currentAngle),
+//                                    desiredState.omegaRadPerSecond);
+      return new SwerveModuleState2(SwerveModuleState.optimize(desiredState, currentAngle), 0);
+//      return desiredState;
+    } else
+    {
+      double lowerBound = currentAngle.getDegrees() - MathUtils.euclideanModulo(currentAngle.getDegrees(), 360);
+      double upperBound = 360 + lowerBound;
+
+      double targetAngle = MathUtils.inputModulo(desiredState.angle.getDegrees(), lowerBound, upperBound);
+      double targetSpeed = desiredState.speedMetersPerSecond;
+      double delta       = targetAngle - currentAngle.getDegrees();
+      if (Math.abs(delta) > 90)
+      {
+        targetSpeed = -targetSpeed;
+        if (delta > 90)
+        {
+          targetAngle -= 180;
+        } else
+        {
+          targetAngle += 180;
+        }
+      }
+      return new SwerveModuleState2(targetSpeed, Rotation2d.fromDegrees(targetAngle), desiredState.omegaRadPerSecond);
+    }
+  }
+
+  /**
+   * Convert to a {@link SwerveModuleState}.
+   *
+   * @return {@link SwerveModuleState} with the same angle and speed.
+   */
+  public SwerveModuleState toSwerveModuleState()
+  {
+    return new SwerveModuleState(this.speedMetersPerSecond, this.angle);
+  }
+}
